531ae2a3885ad067ef993c5e6ab00ddf4bfa5666
bietiekay <bietiekay@d4323bb5-e6f8-f049-b4c0-9a275b4df250>
2007-05-08 17:26:57 +0000

    + added RecordingsThumbnail class to generate a Thumbnail picture for a recording
    git-svn-id: https://svn.schrankmonster.de:8443/svn/YAPS/trunk@174 d4323bb5-e6f8-f049-b4c0-9a275b4df250

--- YAPS_Processors/RecordingsThumbnail.cs
+++ YAPS_Processors/RecordingsThumbnail.cs
@@ -2,14 +2,16 @@ using System;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Drawing.Text;
-using System.Collections.Generic;
 using System.Text;
 
-namespace CreateThumb
+namespace YAPS
 {
-    class Program
+    /// <summary>
+    /// Creates a thumbnail
+    /// </summary>
+    public class RecordingsThumbnail
     {
-        static void Main(string[] args)
+        public void CreateRecordingsThumbnail(Recording recording, String Filename)
         {
             Bitmap newBitmap = new Bitmap(182, 256);
 
@@ -24,23 +26,29 @@ namespace CreateThumb
             objGraphics.FillRegion(Brushes.White, new Region(new Rectangle(0, 0, 182, 256)));
 
             objFont = new System.Drawing.Font("Constantia", 22, FontStyle.Bold);
-            
+
             // load graphic
             //Bitmap stationlogo = new Bitmap("station.png");
 
-            objGraphics.DrawString("16.05.2007", objFont, System.Drawing.Brushes.Black, 15, 62);
-            objGraphics.DrawString("16:30", objFont, System.Drawing.Brushes.Black, 56, 93);
+            objGraphics.DrawString(recording.StartsAt.ToShortDateString(), objFont, System.Drawing.Brushes.Black, 15, 62);
+            objGraphics.DrawString(recording.StartsAt.ToShortTimeString(), objFont, System.Drawing.Brushes.Black, 56, 93);
             objGraphics.DrawString("-", objFont, System.Drawing.Brushes.Black, 86, 120);
-            objGraphics.DrawString("17:00", objFont, System.Drawing.Brushes.Black, 56, 145);
+            objGraphics.DrawString(recording.EndsAt.ToShortTimeString(), objFont, System.Drawing.Brushes.Black, 56, 145);
 
             objGraphics.DrawRectangle(Pens.Black, new Rectangle(45, 207, 101, 21));
 
-            objGraphics.FillRectangle(Brushes.Red, new Rectangle(46, 208, 100, 20));
+            if ( (recording.LastStoppedPosition != 0) && (recording.FileSize != 0) )
+            {
+
+                int percentage = Convert.ToInt32(((float)recording.LastStoppedPosition / (float)recording.FileSize) * 100);
+
+                objGraphics.FillRectangle(Brushes.Red, new Rectangle(46, 208, percentage, 20));
+            }
 
             // Clean up.
             objGraphics.DrawImage(newBitmap, 182, 256);
 
-            newBitmap.Save("test.png", ImageFormat.Png);
+            newBitmap.Save(Filename, ImageFormat.Png);
         }
     }
 }
