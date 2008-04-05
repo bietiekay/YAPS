e3ba90c8a0cf8ce2b429ccee8a2b0eb0d121f093
bietiekay <bietiekay@d4323bb5-e6f8-f049-b4c0-9a275b4df250>
2008-04-05 19:11:55 +0000

    + added WCF Service Host to allow self hosting of data * switched to .NET Framework 3.5
    git-svn-id: https://svn.schrankmonster.de:8443/svn/YAPS/trunk@208 d4323bb5-e6f8-f049-b4c0-9a275b4df250

---
+++ YAPS_Processors/WCF/IYAPSService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.Text;
+
+namespace YAPS.WCF
+{
+    [ServiceContract]
+    public interface IYAPSService
+    {
+        [OperationContract]
+        String YAPSVersion();
+    }
+}
---
+++ YAPS_Processors/WCF/YAPSService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.Text;
+using YAPS;
+
+namespace YAPS.WCF
+{
+    public class YAPSService : IYAPSService
+    {
+        public string YAPSVersion()
+        {
+            return "0.1";
+        }
+    }
+}
